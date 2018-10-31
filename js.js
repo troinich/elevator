27f808eab05e5ea10df27ff02312122e755c9551
Henrik Foogel <henrik.foogel@medieinstitutet.se>
2018-10-31 14:48:09 +0100

    Fixed timer

--- js.js
+++ js.js
@@ -3,70 +3,76 @@ var floor = document.getElementsByClassName("floor");
 var elevator = 0;
 var currentFloor = 0;
 var buttonPushed = 0;
-var myTime = setInterval(moving, 5000);
+var lastFloor;
 
 function OneClicked() {
     if(buttonPushed != 0) {
-        Object.assign(floor[0].style,{backgroundColor:"red"});
-    buttonPushed = 0;
+        Object.assign(floor[0].style,{backgroundColor:"yellow"});
+        buttonPushed = 0;
     }
-    
+    setInterval(moving(), 1000);
 }
 
 function TwoClicked() {
     if(buttonPushed != 1) {
-        Object.assign(floor[1].style,{backgroundColor:"red"});
-    buttonPushed = 1;
+        Object.assign(floor[1].style,{backgroundColor:"yellow"});
+        buttonPushed = 1;
     }
+    setInterval(moving(), 1000);
 }
 
 function ThreeClicked() {
     if(buttonPushed != 2) {
-        Object.assign(floor[2].style,{backgroundColor:"red"});
-    buttonPushed = 2;
+        Object.assign(floor[2].style,{backgroundColor:"yellow"});
+        buttonPushed = 2;
     }
+    setInterval(moving(), 1000);
 }
 
 function FourClicked() {
     if(buttonPushed != 3) {
-        Object.assign(floor[3].style,{backgroundColor:"red"});
-    buttonPushed = 3;
+        Object.assign(floor[3].style,{backgroundColor:"yellow"});
+        buttonPushed = 3;
     }
+    setInterval(moving(), 1000);
 }
 
 function FiveClicked() {
-    if(buttonPushed != 4) {
-        Object.assign(floor[4].style,{backgroundColor:"red"});
-    buttonPushed = 4;
-    }
 
-    moving();
-    
+    if(buttonPushed != 4) {
+        Object.assign(floor[4].style,{backgroundColor:"yellow"});
+        buttonPushed = 4;
+    }
+    setInterval(moving(), 1000);   
 }
 
 function moving() {
 
-    if(currentFloor < buttonPushed) {
-        for(i = currentFloor; i < buttonPushed; i++) {
+    var i = currentFloor;
 
-            Object.assign(floor[i].style,{backgroundColor:"green"});
-        }
-        Object.assign(currentFloor.style,{backgroundColor:"green"});
+    var interval = setInterval(function(){
+
+    console.log("1. i: " + i + " currentFloor: " + currentFloor);
+
+    if (currentFloor == buttonPushed) {
+        clearInterval(interval);
+    } else if(currentFloor <= buttonPushed) {
+        currentFloor++;
+        lastFloor = currentFloor;
+        Object.assign(floor[currentFloor].style,{backgroundColor:"green"});
+        lastFloor--;    
+        Object.assign(floor[(lastFloor)].style,{backgroundColor:"black"});
+        console.log("2. i: " + i + " currentFloor: " + currentFloor + " Button: " + buttonPushed + " Last Upp: " + lastFloor);
+    } else if (currentFloor >= buttonPushed) {
+        currentFloor--;
+        lastFloor = currentFloor;
+        Object.assign(floor[currentFloor].style,{backgroundColor:"green"});
+        lastFloor++;
+        console.log("3. i: " + i + " currentFloor: " + currentFloor + " Button: " + buttonPushed);
+        Object.assign(floor[lastFloor].style,{backgroundColor:"black"});
     }
 
-}
+},1000);
 
-function setDelay(i) {
-    setTimeout(function(){
-        console.log(i);
-    }, 1000);
-}
 
-var tid = setTimeout(mycode, 2000);
-function mycode() {
- // do some stuff...
- tid = setTimeout(mycode, 2000); // repeat myself
-}
-function abortTimer() { // to be called when you want to stop the timer
- clearTimeout(tid);
 }
